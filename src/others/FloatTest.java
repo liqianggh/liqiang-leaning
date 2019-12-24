d3fee23d07427923dbf38445018dd385a46d20b9
liqianggh <liqiang1chn@163.com>
2019-12-25 00:20:33 +0800

    Enhancement: switch和float小测试

---
+++ src/others/FloatTest.java
@@ -0,0 +1,36 @@
+package others;
+
+/**
+ * float测试
+ *
+ * @author Jann Lee
+ * @date 2019-12-25 0:17
+ **/
+public class FloatTest {
+    public static void main(String[] args) {
+        float a = 1.0f - 0.9f;
+        float b = 0.9f - 0.8f;
+
+        if (a == b) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        Float m = Float.valueOf(a);
+        Float n = Float.valueOf(b);
+        if (m.equals(n)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        Double x = new Double(a);
+        Double y = new Double(b);
+        if (x.equals(y)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
