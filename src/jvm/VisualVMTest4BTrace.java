fd453f102717d2a06266c9ce05b1cbe27d6c4975
liqianggh <liqiang1chn@163.com>
2020-02-11 23:23:34 +0800

    Enhancement: JVM诊断工具测试代码

---
+++ src/jvm/VisualVMTest4BTrace.java
@@ -0,0 +1,30 @@
+package jvm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 测试VisualVM
+ *
+ * @author Jann Lee
+ * @date 2020-02-09 22:51
+ **/
+public class VisualVMTest4BTrace {
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) throws IOException {
+        VisualVMTest4BTrace visualVMTest4BTrace = new VisualVMTest4BTrace();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int i = 0; i < 20 ; i++ ){
+            reader.readLine();
+            int a = (int) Math.round(Math.random() * 1000);
+            int b = (int) Math.round(Math.random() * 1000);
+            System.out.println(visualVMTest4BTrace.add(a, b));
+        }
+    }
+
+}
