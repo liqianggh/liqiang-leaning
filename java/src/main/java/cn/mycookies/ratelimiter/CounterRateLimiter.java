2535cd053a0b72e8dd6e65cc9136ac75d9e7b41c
liqianggh <liqiang1chn@163.com>
2021-01-03 22:36:57 +0800

    Enhancement: 固定窗口限流算法
    Signed-off-by: liqiang02 <liqiang02@zhuanzhuan.com>

--- java/src/main/java/cn/mycookies/ratelimiter/CounterRateLimiter.java
+++ java/src/main/java/cn/mycookies/ratelimiter/CounterRateLimiter.java
@@ -1,7 +1,5 @@
 package cn.mycookies.ratelimiter;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 /**
  * 计数 限流器
  * 简单的限流器，维护一个counter， 每秒清零一次
@@ -11,36 +9,36 @@ import java.util.concurrent.atomic.AtomicLong;
  **/
 public class CounterRateLimiter extends MyRateLimiter {
     /**
-     * 计数器
-     */
-    private final AtomicLong counter = new AtomicLong(0);
-    /**
      * 每秒限制请求数
      */
     private final long permitsPerSecond;
     /**
-     * 限流器创建时的初始时间
+     * 上一个窗口的开始时间
      */
     public long timestamp = System.currentTimeMillis();
+    /**
+     * 计数器
+     */
+    private int counter;
 
     public CounterRateLimiter(long permitsPerSecond) {
         this.permitsPerSecond = permitsPerSecond;
     }
 
     @Override
-    public boolean tryAcquire() {
+    public synchronized boolean tryAcquire() {
         long now = System.currentTimeMillis();
+        // 窗口内请求数量小于阈值，更新计数放行，否则拒绝请求
         if (now - timestamp < 1000) {
-            // 1s窗口内 计数器数量小于限制个数，修改计数器，并返回成功
-            if (counter.get() < permitsPerSecond) {
-                counter.incrementAndGet();
+            if (counter < permitsPerSecond) {
+                counter++;
                 return true;
             } else {
                 return false;
             }
         }
-        // 如果间隔超过1s，则认为下一秒开始，设置为0
-        counter.set(0);
+        // 时间窗口过期，重置计数器和时间戳
+        counter = 1;
         timestamp = now;
         return true;
     }
