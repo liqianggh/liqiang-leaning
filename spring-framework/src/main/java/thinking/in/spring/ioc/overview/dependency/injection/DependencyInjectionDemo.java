7fe5beaee2b70ba9525788a27670da690f444e24
liqiang <liqiang1chn@163.com>
2021-04-12 00:44:49 +0800

    NewFeature: 依赖的来源
    Signed-off-by: liqiang <liqiang1chn@163.com>

--- spring-framework/src/main/java/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ spring-framework/src/main/java/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 import thinking.in.spring.ioc.overview.dependency.repository.UserRepository;
 
 
@@ -25,5 +26,13 @@ public class DependencyInjectionDemo {
         final ObjectFactory<ApplicationContext> userFactory = userRepository.getObjectFactory();
         System.out.println(userFactory.getObject() == beanFactory);
 
+        // 依赖的来源
+        // 1. 自定义bean
+        System.out.println(userRepository);
+        // 2. 容器内建Bean
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println(environment);
+        // 3. 依赖注入（内建依赖）
+        System.out.println(userRepository.getBeanFactory() == beanFactory);
     }
 }
