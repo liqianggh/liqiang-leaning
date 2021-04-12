d7fcaf3b227f5a9ebb7b7a509781b8467e9a2c88
liqiang <liqiang1chn@163.com>
2021-04-12 23:41:11 +0800

    NewFeature: 依赖注入 -API
    Signed-off-by: liqiang <liqiang1chn@163.com>

--- spring-framework/src/main/java/thinking/in/spring/bean/definition/AnnotationBeanDefinitionDemo.java
+++ spring-framework/src/main/java/thinking/in/spring/bean/definition/AnnotationBeanDefinitionDemo.java
@@ -1,10 +1,14 @@
 package thinking.in.spring.bean.definition;
 
 import lombok.Data;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import thinking.in.spring.ioc.overview.dependency.domain.User;
 
 import java.util.Map;
@@ -24,17 +28,38 @@ public class AnnotationBeanDefinitionDemo {
         applicationContext.register(Config.class);
         // 启动Spring上下文
         applicationContext.refresh();
-
+        // 一，注解方式注入
         // 1. 通过@Bean方式定义
         // 2. 通过@Component方式定义
         // 3. 通过@Import进行导入
         final Map<String, Config> beansOfType = applicationContext.getBeansOfType(Config.class);
         System.out.println(beansOfType);
         System.out.println(applicationContext.getBean("user"));
+        // 二，api方式注入
+        registerBeanDefinition(applicationContext, User.class);
+
         // 关闭上下文
         applicationContext.close();
     }
 
+    /**
+     * 命名bean的注解方式
+     */
+    public static void registerBeanDefinition(BeanDefinitionRegistry registry, String beanName, Class<?> beanClass) {
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(beanClass);
+        beanDefinitionBuilder.addPropertyValue("id", 123333L);
+        beanDefinitionBuilder.addPropertyValue("name", "hhaha");
+        if (StringUtils.hasText(beanName)) {
+            registry.registerBeanDefinition(beanName, beanDefinitionBuilder.getBeanDefinition());
+        } else {
+            BeanDefinitionReaderUtils.registerWithGeneratedName(beanDefinitionBuilder.getBeanDefinition(), registry);
+        }
+
+    }
+
+    public static void registerBeanDefinition(BeanDefinitionRegistry registry, Class<?> beanClass) {
+        registerBeanDefinition(registry, null, beanClass);
+    }
 
     @Component
     @Data
